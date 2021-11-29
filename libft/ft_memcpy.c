b3e8f5d830557c098682a7454663f652c38c0edb
Nathan FELSEMBERG <nfelsemb@e1r3p4.42.fr>
2021-11-29 18:33:44 +0100

    d

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfelsemb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nfelsemb <nfelsemb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:43:50 by nfelsemb          #+#    #+#             */
-/*   Updated: 2021/11/24 11:43:52 by nfelsemb         ###   ########.fr       */
+/*   Updated: 2021/11/29 18:32:29 by nfelsemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
