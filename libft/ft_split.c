1e8b31aa1de12f665235c4beeeaf774df01037bd
Nathan FELSEMBERG <nfelsemb@e1r4p8.42.fr>
2021-11-30 17:06:06 +0100

    d

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -11,11 +11,53 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
+char	**tabvide(const char *s, char c)
+{
+	int		i;
+	int		j;
+	char	**de;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			j++;
+		i++;
+	}
+	de = malloc(sizeof(char *) * (j + 1));
+	if (!de)
+		return (0);
+	de[j] = 0;
+	return (de);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	printf("%s", s);
-	printf("%c", c);
-	return (0);
+	int		i;
+	int		j;
+	int		k;
+	char	**dest;
+
+	i = 0;
+	k = 0;
+	dest = tabvide(s, c);
+	if (!dest)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			j = i;
+			while (s[j] != c && s[j])
+				j++;
+			dest[k ++] = ft_substr(s, i, j - i);
+			i = j - 1;
+		}
+		if (s[i + 1] == 0)
+			break ;
+		i++;
+	}
+	return (dest);
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nfelsemb <nfelsemb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:06:22 by nfelsemb          #+#    #+#             */
-/*   Updated: 2021/11/30 14:58:12 by nfelsemb         ###   ########.fr       */
+/*   Updated: 2021/11/30 16:01:47 by nfelsemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,11 @@ int	count(char const *s1, char const *set)
 		i++;
 		count++;
 	}
+	if (!s1[i])
+		return (count);
 	while (s1[i])
 		i++;
+	i--;
 	while (!isd(s1[i], set))
 	{
 		count++;
