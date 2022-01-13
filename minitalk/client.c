904dcf9b24c9821c832d394d16b2f680039584b7
Nathan FELSEMBERG <nfelsemb@e1r13p2.42.fr>
2022-01-13 15:13:49 +0100

    ta

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: nfelsemb <nfelsemb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 16:28:41 by nfelsemb          #+#    #+#             */
-/*   Updated: 2022/01/04 16:02:25 by nfelsemb         ###   ########.fr       */
+/*   Updated: 2022/01/06 15:03:35 by nfelsemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,59 @@
 #include <sys/types.h>
 #include <signal.h>
 
+void	checkerr(int err)
+{
+	if (err == -1)
+	{
+		ft_printf("Erreur de transmition !");
+		exit(2);
+	}
+}
+
+void	ft_send(char *str, int pid)
+{
+	int	i;
+	int	j;
+	int	b;
+	int	ret;
+
+	j = 0;
+	b = 1;
+	while (b)
+	{
+		if (str[j] == '\0')
+			b = 0;
+		i = 7;
+		while (i != -1)
+		{
+			if ((str[j] >> i) % 2)
+				ret = kill(pid, SIGUSR2);
+			else
+				ret = kill(pid, SIGUSR1);
+			i--;
+			checkerr(ret);
+			usleep(100);
+		}
+		j++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	pid;
 
 	if (argc != 3)
 	{
-		ft_printf("Erreur d'argument");
+		ft_printf("Erreur d'argument\n");
 		exit(2);
 	}
 	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
+	if (pid <= 0)
+	{
+		ft_printf("Le pid ne peut pas etre de 0 ou inferieur !\n");
+		exit(3);
+	}
+	ft_printf("Envoie du message sur le pid : %d\n", pid);
+	ft_send(argv[2], pid);
+	ft_printf("Message envoye !\n");
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,30 +6,77 @@
 /*   By: nfelsemb <nfelsemb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 16:28:43 by nfelsemb          #+#    #+#             */
-/*   Updated: 2022/01/04 16:00:30 by nfelsemb         ###   ########.fr       */
+/*   Updated: 2022/01/06 13:31:16 by nfelsemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
 #include <signal.h>
+#include "libft/libft.h"
 
-typedef void	(*t_sighandler)(int);
+char	g_c;
+
+char	*ft_strjoinchar(char const *s1, char const s2)
+{
+	int		i;
+	int		len1;
+	char	*dest;
+
+	len1 = ft_strlen(s1);
+	dest = malloc(sizeof(char) * (len1 + 2));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	dest[len1] = s2;
+	len1++;
+	dest[len1] = '\0';
+	return (dest);
+}
 
 static void	sigrec(int sig)
 {
-	ft_printf("ok\n");
-	(void) sig;
+	static int		i;
+	static char		*tmp;
+
+	if (!tmp)
+	{
+		tmp = ft_calloc(1, sizeof(char));
+		i = 8;
+	}
+	g_c = g_c << 1;
+	if (sig == SIGUSR2)
+		g_c++;
+	if (i == 1)
+	{
+		if (g_c != '\0')
+			tmp = ft_strjoinchar(tmp, g_c);
+		else
+		{
+			ft_printf("%s\nEn attente de message ...\n", tmp);
+			free(tmp);
+			tmp = ft_calloc(1, sizeof(char));
+		}
+		i = 8;
+	}
+	else
+		i--;
 }
 
 int	main(void)
 {
 	ft_printf("---------------------------\n-                         -\n");
-	ft_printf("-   Pid serveur : %d   -\n", getpid());
+	ft_printf("-   Pid serveur : %d    -\n", getpid());
 	ft_printf("-                         -\n---------------------------\n");
-	sleep(5);
+	ft_printf("En attente de message ...\n");
+	g_c = 0;
 	signal(SIGUSR1, sigrec);
+	signal(SIGUSR2, sigrec);
 	while (1)
-	{
 		pause();
-	}
 }
